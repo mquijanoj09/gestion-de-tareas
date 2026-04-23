3447e6e11d584be0301b8f07732fba84a2ec549a
mquijanoj09 <mquijanoj@gmail.com>
2026-04-22 21:29:06 -0500

    Fix: CD Pipeline fix 2

--- infra/terraform/modules/compute/main.tf
+++ infra/terraform/modules/compute/main.tf
@@ -74,7 +74,7 @@ resource "aws_instance" "app" {
   user_data                   = local.user_data
 
   root_block_device {
-    volume_size = 20
+    volume_size = 30
     volume_type = "gp3"
   }
 
